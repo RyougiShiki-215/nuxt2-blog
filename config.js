2d85a5ae6c0f6f18d7cfcbb2ba22d282634bd90d
RyougiShiki-215 <92664437+RyougiShiki-215@users.noreply.github.com>
2022-06-29 16:25:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: '个人博客', // 网站标题
+  title: 'Ryougi的个人博客', // 网站标题
   nickName: "Ryougi", // 昵称
   githubName: 'RyougiShiki-215', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-    SEO_description: 'Ryougi的个人博客',
+    SEO_description: '是Ryougi的个人博客捏~',
   SEO_keywords: 'Ryougi,博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
